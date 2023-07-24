0873bba2f80036ce148d49d00b49b0328c427e7d
Micaela Zerpa <micazerpa14@gmail.com>
2023-07-24 19:19:13 -0300

    Estructura de archivos utilizando Node.js+Express

---
+++ app.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+app.use(express.static('public'));
+
+const port=3000;
+
+app.listen(port, ()=>(console.log(`Server up: port:${port}`)));
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
+})
