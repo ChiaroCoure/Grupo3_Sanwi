cdbf5e09784359ab567b90108038122bd9727684
Gabriel <gamcode98@gmail.com>
2023-08-22 17:40:38 -0300

    fix css path

--- dataBase/products.js
+++ dataBase/products.js
@@ -4,21 +4,21 @@ const products=[
         name:'Tostado de jamón y queso',
         description:'Pan de semillas de masa madre, jamón cocido Las Dinas, queso gruyère y aceite de oliva Zuelo Orgánico.Con papas fritas o mix de verdes hidropónicos',
         price:1450.50,
-        img:'sandwich-de-jamon-crudo-y-burrata.webp'
+        img:'sandwich-de-jamon-crudo-y-burrata.jpg'
     },
     {
         id:'2',
         name:'Sandwich de jamón crudo y burrata',
         description:'Pan de semillas de masa madre, jamón cocido Las Dinas, queso gruyère y aceite de oliva Zuelo Orgánico.Con papas fritas o mix de verdes hidropónicos',
         price:1450.50,
-        img:'sandwich-de-jamon-crudo-y-burrata.webp'
+        img:'sandwich-de-jamon-crudo-y-burrata.jpg'
     },
     {
         id:'3',
         name:'Milanesa',
         description:'Pan de semillas de masa madre, jamón cocido Las Dinas, queso gruyère y aceite de oliva Zuelo Orgánico.Con papas fritas o mix de verdes hidropónicos',
         price:1450.50,
-        img:'sandwich-de-jamon-crudo-y-burrata.webp'
+        img:'sandwich-de-jamon-crudo-y-burrata.jpg'
     }
 ]
 
