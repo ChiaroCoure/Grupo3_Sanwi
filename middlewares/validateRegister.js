192665604b603f40c4b6a848de6c0dde145fd98a
Micaela Zerpa <micazerpa14@gmail.com>
2023-09-07 09:34:19 -0300

    sin validator

--- middlewares/validateRegister.js
+++ middlewares/validateRegister.js
@@ -3,13 +3,14 @@ const {body, validationResult} = require('express-validator');
 const arrRegister=[
     body('username').notEmpty().withMessage('Debe ingresar un nombre de usuario'),
     body('email').notEmpty().withMessage('Debe ingresar tu email').bail().isEmail().withMessage('Debes ingresar un formato de email válido'),
-    body('password').notEmpty().withMessage('Debe ingresar tu password'),
-    body('password').isLength({min:5, max:8}).withMessage('Debe ingresar una cantidad min de 5 y un max de 8 caracteres')
+    body('password').notEmpty().withMessage('Debe ingresar tu password')
+    /* .isLength({min:5, max:8}).withMessage('Debe ingresar una cantidad min de 5 y un max de 8 caracteres') */
     
 ];
 
 const validateRegister=(req, res, next)=>{
     const errors=validationResult(req);
+    console.log(errors)
     if(errors.isEmpty()===true){
         next()
     }else{
--- routers/users.js
+++ routers/users.js
@@ -28,6 +28,6 @@ router.get('/login', userController.login)
 router.post('/login', validateUser, userController.render)
 
 router.get('/register', userController.register)
-router.post('/register', arrRegister, validateRegister, upload.single('image'), userController.store);  
+router.post('/register', upload.single('image'), userController.store);  
 
 module.exports=router;
