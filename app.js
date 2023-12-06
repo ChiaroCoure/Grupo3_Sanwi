38d64ffbe00ed7b600f000aaa0011eab1b0a0132
Gabriel <gamcode98@gmail.com>
2023-12-06 10:53:49 -0300

    add cors

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, './public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use(methodOverride('_method'));
 app.use(session({
   secret: 'sanwi cat',
