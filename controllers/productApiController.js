18516e2fcb5e6bb96727aa60f150bc0077e7d292
Micaela Zerpa <micazerpa14@gmail.com>
2023-12-12 12:01:07 -0300

    category products

--- controllers/productApiController.js
+++ controllers/productApiController.js
@@ -3,10 +3,17 @@ const db = require('../dataBase/models');
 const productApiController = {
   getProducts: async function (req, res) {
 
-    const products = await db.Product.findAll()
-    const productsParsed=products.map(product=>({...product.dataValues, image: `http://localhost:3000/img/products/${product.image}`}))
-    res.status(200).json(productsParsed); 
-    
+    const products = await db.Product.findAll({
+      include: [
+        {
+          association: 'category',
+          attributes: ['id', 'name']
+        }
+      ]
+    })
+    const productsParsed = products.map(product => ({ ...product.dataValues, image: `http://localhost:3000/img/products/${product.image}` }))
+    res.status(200).json(productsParsed);
+
   }
 }
 
